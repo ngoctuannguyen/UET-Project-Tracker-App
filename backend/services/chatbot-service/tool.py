92383addc7266d344352900622236bff33e3674d
ngoctuannguyen1980123@gmail.com <ngoctuannguyen1980123@gmail.com>
2025-05-11 15:41:37 +0700

    delete: remove

--- backend/services/chatbot-service/tool.py
+++
@@ -1,5 +0,0 @@
-from script import send_reports
-
-image_url = "C:/Users/ADMIN/Documents/code/UET-Project-Tracker-App/uploads/barcode_with_product.png"
-content = "This is a test report content."
-print(send_reports(image_url, content))
