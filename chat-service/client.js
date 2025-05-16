2d108ca71459d1817b1b53a85a4113863d18fc49
TDThuyen <22026505@vnu.edu.vn>
2025-05-16 22:19:29 +0700

    update chat-service

--- chat-service/client.js
+++ chat-service/client.js
@@ -1,8 +1,8 @@
-const readline = require('readline');
-const { io } = require('socket.io-client');
+const readline = require("readline");
+const { io } = require("socket.io-client");
 
 // Kết nối đến server WebSocket
-const socket = io('http://localhost:3000');
+const socket = io("http://localhost:3002");
 
 // Tạo giao diện nhập liệu trên terminal
 const rl = readline.createInterface({
@@ -10,29 +10,33 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-let username = '';
+let username = "";
 
 // Nhập tên người dùng
-rl.question('Enter your username: ', (name) => {
+rl.question("Enter your username: ", (name) => {
   username = name;
   group_id = "d8f08250-555b-4e90-9a1b-473940951b22";
   console.log(`Welcome, ${username} to group ${group_id}! Start chatting...\n`);
   user_id = "111";
   // Lắng nghe tin nhắn từ server
-  socket.on('receive-message', (data) => {
+  socket.on("receive-message", (data) => {
     if (data.username !== username) {
       console.log(`${data.username}: ${data.message}`);
     }
   });
 
   // Nhập tin nhắn
-  rl.on('line', (message) => {
-    socket.emit('send-message', { group_id: group_id, sender_name: username, text: message, sender_id: "111"});
+  rl.on("line", (message) => {
+    socket.emit("send-message", {
+      group_id: group_id,
+      sender_name: username,
+      text: message,
+      sender_id: "111",
+    });
   });
 
   rl.on("close", (user_id) => {
     socket.disconnect();
     console.log("Disconnected from the server.");
   });
-
 });
